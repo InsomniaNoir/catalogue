75c4ae504204c859d60b99694529ef64d8e248f4
Jeremiah Shafer <jrshafer@mac.com>
2015-08-06 18:37:37 +0000

    Add div tag errors to _form.html.erb

--- app/models/verbiage.rb
+++ app/models/verbiage.rb
@@ -2,5 +2,10 @@ class Verbiage < ActiveRecord::Base
     belongs_to :category
     belongs_to :user
     
-    default_scope { order('created_at DESC') } 
+    default_scope { order('created_at DESC') }
+    
+    validates :title, length: { minimum: 5 }, presence: true
+    validates :body, length: { minimum: 20 }, presence: true
+    validates :category, presence: true
+    validates :user, presence: true
 end
